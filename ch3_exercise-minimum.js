0ec8089df5103111f114a232182994ca4e3e1cd4
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-03-20 11:41:10 +0800

    Update minimum

--- ch3_exercise-minimum.js
+++ ch3_exercise-minimum.js
@@ -1,5 +1,13 @@
-function min(arg1, arg2) {
-  return Math.min(arg1, arg2);
+// function min(arg1, arg2) {
+//   return Math.min(arg1, arg2);
+// }
+
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
 }
 
-console.log(min(1, 4));
+console.log(min(0, 10)); // 0
+console.log(min(0, -10)); // 10
